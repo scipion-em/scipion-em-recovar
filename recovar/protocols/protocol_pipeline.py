e85a2e42e90be7dd23312010f6b6b7e4083e1b21
Ratolon <miceta@cnb.csic.es>
2025-09-01 14:26:43 +0000

    First version of the pipeline protocol

--- recovar/protocols/protocol_pipeline.py
+++ recovar/protocols/protocol_pipeline.py
@@ -25,3 +25,137 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
+
+from enum import Enum
+from os.path import dirname, basename
+from os import symlink
+
+from pyworkflow.constants import BETA
+from pyworkflow.protocol.constants import LEVEL_ADVANCED
+import pyworkflow.protocol.params as params
+from pyworkflow.utils import Message
+from pyworkflow.utils.path import createLink
+
+from pwem.objects import SetOfParticles, SetOfParticlesFlex, VolumeMask
+from pwem.protocols import EMProtocol
+
+from recovar.convert import writeMetadata
+from recovar import Plugin
+
+class outputs(Enum):
+    particles = SetOfParticlesFlex
+
+class RecovarPipeline(EMProtocol):
+    _label = 'pipeline'
+    _devStatus = BETA
+    _possibleOutputs = outputs
+    
+    def _defineParams(self, form: params.Form):
+        """ Define the input parameters that will be used.
+        Params:
+            
+        """
+        form.addSection(label=Message.LABEL_INPUT)
+
+        form.addHidden(params.USE_GPU, params.BooleanParam, default=True,
+                           expertLevel=LEVEL_ADVANCED,
+                           label="Use GPU?",
+                           help="Set to True if you want to use GPU implementation."
+                           )
+        
+        form.addHidden(params.GPU_LIST, params.StringParam, default='0',
+                       expertLevel=LEVEL_ADVANCED,
+                       label="Choose GPU IDs",
+                       help="GPU may have several cores. Set it to zero"
+                            " if you do not know what we are talking about."
+                            " First core index is 0, second 1 and so on."
+                            " Recovar can use multiple GPUs - in that case"
+                            " set to i.e. *0 1 2*."
+                            )
+
+        form.addParam('inputParticles', params.PointerParam,
+                      pointerClass = SetOfParticles,
+                      pointerCondition = 'hasAlignmentProj', # TODO: Add validation for CTF in the validation section because aca no se puede
+                      important = True,
+                      label = 'Input particles',
+                      allowsNull = False,
+                      help = ''
+                      )
+        
+        form.addParam('solventMask', params.PointerParam,
+                      pointerClass = VolumeMask,
+                      label = 'Solvent mask',
+                      allowsNull = False,
+                      help = ''
+                      )
+        
+        form.addParam('focusMask', params.PointerParam,
+                      pointerClass = VolumeMask,
+                      label = 'Focus mask',
+                      allowsNull = True,
+                      help = ''
+                      )
+
+    # --------------------------- STEPS functions ------------------------------
+
+    def _insertAllSteps(self):
+        # Insert processing steps
+        self._insertFunctionStep(self.convertInputStep)
+        self._insertFunctionStep(self.runRecovarStep)
+        #self._insertFunctionStep(self.createOutputStep)
+
+    def convertInputStep(self):
+        # Convert the inputs for Recovar
+        writeMetadata(self.inputParticles.get(),
+                      self._getPosesFilename(),
+                      self._getCTFFilename(),
+                      self._getImagesFilename())
+        # HACK to avoid Recovar bug with relative paths
+        files = self.inputParticles.get().getFiles()
+        for file in files:
+            #symlink(file, self._getExtraPath(basename(file)))
+            createLink(file, self._getExtraPath(basename(file)))
+        
+    def runRecovarStep(self):
+        # Prepare command line
+        program = 'recovar'
+
+        # Fill arguments
+        args = []
+        args.append('pipeline')
+        args.append(self._getImagesFilename())
+        args += ['-o', self._getExtraPath()]
+        args += ['--poses', self._getPosesFilename()]
+        args += ['--ctf', self._getCTFFilename()]
+        args += ['--mask', self.solventMask.get().getFileName()]
+
+        if self.focusMask.get() is not None:
+            args += ['--focus-mask', self.focusMask.get().getFileName()]
+
+        # Run
+        Plugin.runRecovar(self, program, args)
+        
+    def createOutputStep(self):
+        pass
+        self._defineOutputs(**{outputs.count.name: timesPrinted})
+        self._defineSourceRelation(self.message, timesPrinted)
+
+    # --------------------------- INFO functions ---------------------------------
+    # --------------------------- UTILS functions --------------------------------
+    
+    def _getImagesFilename(self):
+        return self._getExtraPath('images.star')
+    
+    def _getPosesFilename(self):
+        return self._getExtraPath('poses.pkl')
+    
+    def _getCTFFilename(self):
+        return self._getExtraPath('ctf.pkl')
+    
+    # THIS IS A TEMPORAL APAÑO until recovar gets this fixed
+    def _getDataDirHack(self):
+        datadirs = set(map(dirname, self.inputParticles.get().getFiles()))
+        if len(datadirs) != 1:
+            self.error("Input particles should come from the same directory")
+        else:
+            return datadirs.pop()    
